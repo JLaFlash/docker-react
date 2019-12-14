1b8173d8c2fc861967ec58bfbf1a0ecb5ac16ee1
JLaFlash <jodylaflash@outlook.com>
2019-12-14 10:24:53 -0600

    Add travis ci

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World from Jody LaFlash
+          Hello World from Terrie Laflash
         </p>
         <a
           className="App-link"
